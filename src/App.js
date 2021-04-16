1f628361480c0f627ccaf9bbfc896c2411146500
Abdqadr1 <abolarinwaquadri@gmail.com>
2021-04-16 13:25:48 -0700

    home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/home';
+import Solution from './components/solution';
 
 function App() {
   return (
-    <div className="App">
-     
+    <BrowserRouter>
+      <div className="App">
+      <Switch>
+        <Route  exact path="/" component={Home}/>
+        <Route path="/solution" component={Solution} />
+      </Switch>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,21 @@
+
+import { Link } from 'react-router-dom';
+import err from '../images/err1.jpg';
+import arrow from '../images/Arrows_down_animated.gif';
 const Home = () => {
-    return ( <div className="container">
+    return ( <div className="home-container">
+            <div className="arrow-div"><img src={arrow} alt="arrow down"/></div>
+             <div className="box">
+                <h1 className="header">VITOLIZE MEN</h1>
+                <div className="flex-box">
+                    <img src={err} alt="satisfy madam"/>
+                    <p className="welcome-text">
+                        Welcome To Men Health Solution Platform. New Natural Remedy To Improve Your Strenght In The Inner Room, Feel Younger Again.
+                        Just Try This Natural Booster.
+                    </p>
+                </div>
+                <button className="visit-btn"><Link to='/solution'>VISIT WEBSITE</Link></button> 
+            </div>
         
     </div> );
 }
